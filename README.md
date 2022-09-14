b3c99d070c01ea2da0a812bbe36e1964458140d6
karenL26 <karen.lima@estudiantes.utec.edu.uy>
2022-09-14 06:14:05 +0000

    project completed

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Template for Machine Learning projects
+# Time Series Project
+> 4 Geeks Academy - project by @karenL26 :elf:
+
+In this project we will deal with time series forecasting by creating a cpu usage anomaly detection model. We are going to create two models, one using ARIMA and the other using Prophet.
+
+In this project you can find some basic exploratory analysis and the data preparation for modeling [here](src/explore_tsf.ipynb).
+
+The model construction use XGBoost and it was applied to it hypertune. 
+
+The model construction is [here](src/app.py).
+
+The datasets were obtained from the following links: 
+* [cpu_train_a](https://raw.githubusercontent.com/oreilly-mlsec/book-resources/master/chapter3/datasets/cpu-utilization/cpu-train-a.csv)
+* [cpu_train_b](https://raw.githubusercontent.com/oreilly-mlsec/book-resources/master/chapter3/datasets/cpu-utilization/cpu-train-b.csv)
+* [cpu_test_a](https://raw.githubusercontent.com/oreilly-mlsec/book-resources/master/chapter3/datasets/cpu-utilization/cpu-test-a.csv)
+* [cpu_test_b](https://raw.githubusercontent.com/oreilly-mlsec/book-resources/master/chapter3/datasets/cpu-utilization/cpu-test-b.csv)
--- src/app.py
+++ src/app.py
@@ -1,4 +1,73 @@
-from utils import db_connect
-engine = db_connect()
+#####################################
+#       Time Series Project         #
+#####################################
+### Load libraries and modules ###
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+import joblib
+import os
+from prophet import Prophet
+from pmdarima.arima import auto_arima
+from pylab import rcParams
 
-# your code here
+
+######################
+# Data Preprocessing #
+######################
+# Loading the datasets
+cpu_train_a_raw = pd.read_csv('https://raw.githubusercontent.com/oreilly-mlsec/book-resources/master/chapter3/datasets/cpu-utilization/cpu-train-a.csv')
+cpu_train_b_raw = pd.read_csv('https://raw.githubusercontent.com/oreilly-mlsec/book-resources/master/chapter3/datasets/cpu-utilization/cpu-train-b.csv')
+cpu_test_a_raw = pd.read_csv('https://raw.githubusercontent.com/oreilly-mlsec/book-resources/master/chapter3/datasets/cpu-utilization/cpu-test-a.csv')
+cpu_test_b_raw = pd.read_csv('https://raw.githubusercontent.com/oreilly-mlsec/book-resources/master/chapter3/datasets/cpu-utilization/cpu-test-b.csv')
+
+# Create a copy of the original datasets
+cpu_train_a = cpu_train_a_raw.copy()
+cpu_train_b = cpu_train_b_raw.copy()
+cpu_test_a = cpu_test_a_raw.copy()
+cpu_test_b = cpu_test_b_raw.copy()
+
+#####################
+#    Dataset A      #
+#####################
+# Convert the dataframe index to a datetime index 
+cpu_train_a['datetime'] = pd.to_datetime(cpu_train_a['datetime'])
+cpu_train_a = cpu_train_a.set_index('datetime')
+cpu_test_a['datetime'] = pd.to_datetime(cpu_test_a['datetime'])
+cpu_test_a = cpu_test_a.set_index('datetime')
+
+### Model using ARIMA ###
+stepwise_model_a = auto_arima(cpu_train_a, start_p=1, start_q=1,
+                           max_p=3, max_q=3, m=12,
+                           start_P=0, seasonal=True,
+                           d=1, D=1, trace=True,
+                           error_action='ignore',  
+                           suppress_warnings=True, 
+                           stepwise=True)
+print(stepwise_model_a.aic())
+stepwise_model_a.fit(cpu_train_a)
+future_forecast = stepwise_model_a.predict(n_periods=60)
+future_forecast = pd.DataFrame(future_forecast,index = cpu_test_a.index,columns=['Prediction'])
+print('Future forecast dataset A using ARIMA:\n')
+print(future_forecast)
+# We save the model with joblib
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, '../models/arima_ds_a.pkl')
+joblib.dump(stepwise_model_a, filename)
+
+#####################
+#    Dataset B      #
+#####################
+# Rename the columns to use prophet
+data_train_b = cpu_train_b_raw.copy()
+data_train_b.rename(columns={'datetime': 'ds', 'cpu': 'y'}, inplace=True)
+m_b = Prophet()
+m_b.fit(data_train_b)
+future = m_b.make_future_dataframe(periods=1)
+print('Future forecast dataset B using Prophet:\n')
+print(future.tail())
+
+# We save the model with joblib
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, '../models/prophet_ds_b.pkl')
+joblib.dump(m_b, filename)
